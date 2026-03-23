20526a5e0b5340962a182fc59f11c8a676327d5b
maximMaximson <drleorus@mail.ru>
2026-03-23 13:31:19 +0300

    123

---
+++ js/inputController-test.js
@@ -0,0 +1,105 @@
+const attachBtn = document.getElementById("attach_btn");
+const activateBtn = document.getElementById("activate_btn");
+const bindJumpToSpaceBtn = document.getElementById("bindJumpToSpace");
+
+const inputController = new InputController();
+
+var isAttached = false;
+var isActive = false;
+var isBinded = false;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+attachBtn.addEventListener("click", () => {
+    if (isAttached){
+        attachBtn.innerHTML = "Аттач к элементу DOM";
+        inputController.detach();
+        isAttached = false;
+    } else {
+        attachBtn.innerHTML = "Деттач от элемента DOM";
+        inputController.attach(canvas);
+        isAttached = true;
+    }
+});
+
+activateBtn.addEventListener("click", () => {
+    if (isActive){
+        activateBtn.innerHTML = "Активация контроллера";
+        inputController.enabled = false;
+        isActive = false;
+    } else {
+        activateBtn.innerHTML = "Деактивация контроллера";
+        inputController.enabled = true;
+        isActive = true;
+    }
+});
+
+bindJumpToSpaceBtn.addEventListener("click", () => {
+    if (!isBinded){
+        bindJumpToSpaceBtn.innerHTML = "Отвязать доп. активность 'прыжок' от кнопки 'пробел'";
+        console.log("bind jump to space");
+        inputController.bindActions([{name : "jump", keys: ["Space"], enabled : true}]);
+        isBinded = true;
+    } else{
+        bindJumpToSpaceBtn.innerHTML = "Байнд доп. активности 'прыжок' на кнопку 'пробел'";
+        console.log("unbind jump to space");
+        inputController.bindActions([{name : "jump", keys: ["Space"], enabled : false}]);
+        isBinded = false;
+    }
+});
+
+inputController.bindActions([
+    {name : "left", keys: ["ArrowLeft", "KeyA"], enabled : true},
+    {name : "right", keys: ["ArrowRight", "KeyD"], enabled : true},
+    {name : "up", keys: ["ArrowUp", "KeyW"], enabled : true},
+    {name : "down", keys: ["ArrowDown", "KeyS"], enabled : true}
+]);
+
+
+
+function gameLoop(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleJump();
+
+    handleMovement();
+
+    draw();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+
+function draw(){
+    var circle = new Path2D();
+    circle.arc(x, y, 25, 0, 2 * Math.PI);
+    ctx.fill(circle);
+}
+
+function handleMovement(){
+    if (inputController.isActionActive("left")){
+        x -= 1;
+    }
+    if (inputController.isActionActive("right")){
+        x += 1;
+    }
+    if (inputController.isActionActive("up")){
+        y -= 1;
+    }
+    if (inputController.isActionActive("down")){
+        y += 1;
+    }
+}
+
+function handleJump(){
+    if (inputController.isActionActive("jump")){
+        console.log("jump");
+        ctx.fillStyle = "blue";
+    } else {
+        ctx.fillStyle = "red";
+    }
+}
+
---
+++ js/inputController.js
@@ -0,0 +1,74 @@
+class InputController {
+    static get ACTION_ACTIVATED() { return "input-controller:activate" };
+    static get ACTION_DEACTIVATED() { return "input-controller:deactivate" };
+
+    _enabled = false;
+    _focused = false;
+    _keys_active = new Map();
+    _actions = new Map();
+
+    constructor(actionsToBind = [], target = null) {
+        if (actionsToBind) {
+            this.bindActions(actionsToBind);
+        }
+        if (target) {
+            this.attach(target);
+        }
+
+        this.bindEvents();
+    }
+
+    bindActions(actionsToBind) {
+        for (const action of actionsToBind) {
+            this._actions.set(action.name,{keys: new Set(action.keys), enabled: action.enabled});
+        }
+    }
+
+    enableAction(actionName) {
+        this._actions.get(actionName).enabled = true;
+    }
+
+    disableAction(actionName) {
+        this._actions.get(actionName).enabled = false;
+    }
+
+    attach(target, dontEnable = false) {
+        this._target = target;
+        this._enabled = !dontEnable;
+    }
+
+    detach() {
+        this._target = null;
+    }
+
+    isActionActive(actionName) {
+        if (!this._enabled || !this._focused) {
+            return false;
+        }
+        if (!this._actions.get(actionName)) {
+            return false;
+        }
+        for (const key of this._actions.get(actionName).keys) {
+            if (this._keys_active.get(key) && this._actions.get(actionName).enabled) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isKeyPressed(keyCode) {
+        return this._keys_active.get(keyCode);
+    }
+
+
+    bindEvents() {
+        window.addEventListener("keydown", (e) => {
+            this._keys_active.set(e.code, true);
+        });
+        window.addEventListener("keyup", (e) => {
+            this._keys_active.set(e.code, false);
+        });
+    }
+
+
+}
